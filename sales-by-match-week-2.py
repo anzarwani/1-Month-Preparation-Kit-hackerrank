37ed6d951b16afc1f6a46f6f2eeaf5f0d09ecc4a
Anzar <43462815+anzarwani@users.noreply.github.com>
2023-11-28 22:06:52 +0530

    week-2

---
+++ sales-by-match-week-2.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    count = {}
+    
+    for num in ar:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+    result = [value // 2 for value in count.values()]
+
+    return(sum(result))
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
